dfd11925bb6215a69e9b8ed1231690a34f9c16a5
Mike <wwwtitov@yandex.ru>
2024-01-15 22:46:56 +0300

    DZ 2 Task 1 done

---
+++ HomeWork/dz2/Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Input first number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void multiple(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine("Кратно 7 и 23 ");
+    }
+    else
+    {
+        System.Console.WriteLine("Не кратно 7 и 23 ");
+    }
+}
+
+multiple(number);
