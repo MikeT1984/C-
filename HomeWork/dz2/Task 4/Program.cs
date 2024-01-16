bb99012d8b3ca16b5cba4459e38903a602fca794
Mike <wwwtitov@yandex.ru>
2024-01-16 22:17:54 +0300

    DZ 2 Task 4 done

---
+++ HomeWork/dz2/Task 4/Program.cs
@@ -0,0 +1,16 @@
+﻿void DisplayDigits(int number)
+{
+    Console.Write("Цифры числа через запятую: ");
+
+    string numberString = number.ToString();
+
+    for (int i = 0; i < numberString.Length; i++)
+    {
+        Console.Write(numberString[i]);
+        if (i < numberString.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+DisplayDigits(444);
