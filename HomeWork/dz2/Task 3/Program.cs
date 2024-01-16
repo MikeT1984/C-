8307f864aa0c180a9f262e66411bc553cfa8c7aa
Mike <wwwtitov@yandex.ru>
2024-01-16 21:39:22 +0300

    DZ 2 Task 3 done

---
+++ HomeWork/dz2/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿ void maxNumber(int num)
+    {
+        int max = 0;
+
+        if (10 <= num && num <= 99)
+        {
+            int d1 = num % 10; // Последняя цифра
+            int d2 = num / 10; // Первая цифра
+
+            if (d1 > d2)
+            {
+                max = d1;
+            }
+            else
+            {
+                max = d2;
+            }
+
+            Console.WriteLine("max = " + max);
+        }
+        else
+        {
+            Console.WriteLine("Некорректное число");
+        }
+    }
+maxNumber(12);
