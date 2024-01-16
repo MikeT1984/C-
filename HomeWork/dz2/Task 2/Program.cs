9d31744811eed9bee2dc7b205783e0cbeeff1df2
Mike <wwwtitov@yandex.ru>
2024-01-16 20:40:09 +0300

    DZ 2 Task 2 done

---
+++ HomeWork/dz2/Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿void plane(int x, int y)
+{
+if (x > 0 && y > 0){
+ Console.WriteLine("1");
+}
+if (x < 0 && y > 0){
+  Console.WriteLine("2");
+}
+if (x < 0 && y < 0){
+  Console.WriteLine("3");
+  }
+if (x > 0 && y < 0){
+  Console.WriteLine("4");
+}
+}
+
+plane(4,-6);
