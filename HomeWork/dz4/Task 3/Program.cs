ba0e99f7cffb73c7e265472b2837bb84d1326a88
Mike <wwwtitov@yandex.ru>
2024-01-21 20:48:14 +0300

    add Task 3

---
+++ HomeWork/dz4/Task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿static int[] GenerateRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+static void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+
+static void PrintResult(int[] array)
+{
+    Console.WriteLine("Сгенерированный массив: " + string.Join(", ", array));
+
+    ReverseArray(array);
+
+    Console.WriteLine("Реверсивный массив: " + string.Join(", ", array));
+}
+
+PrintResult(GenerateRandomArray(10, 100, 200));
