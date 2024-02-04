9806ab362247aec60cec5ef8d888f923c46e57d8
Mike <wwwtitov@yandex.ru>
2024-02-04 22:00:21 +0300

    add dz7

---
+++ HomeWork/dz7/Tsk1/Program.cs
@@ -0,0 +1,9 @@
+﻿    static void PrintNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbersInRange(m + 1, n);
+        }
+    }
+PrintNumbersInRange(2,10);
---
+++ HomeWork/dz7/Tsk3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+void PrintArrayReversed(int[] array,int index){
+if (index < 0){
+    return;
+}
+Console.WriteLine(array[index]);
+PrintArrayReversed(array,index-1);
+
+}
+int[] array ={1,2,4,5,6,7,8};
+PrintArrayReversed(array,array.Length-1);
